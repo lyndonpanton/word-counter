acdaa8d87304232409c1aaa113cd0bef4d59ac39
Lyndon Panton <LyndonPanton@gmail.com>
2025-05-06 15:49:45 +0100

    Added the metadata

---
+++ web/index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE HTML>
+<html lang="en-GB">
+<head>
+    <!-- meta -->
+    <!-- charset, application-name author, description, keywords, viewport -->
+    <meta charset="UTF-8" />
+    <meta name="application-name" content="word-counter" />
+    <meta name="author" content="Lyndon Mykal Panton" />
+    <meta name="description" content="An application which allows users to view
+            statistics about text they supply to the application, such as the
+            word count, character count, space counter, word occurrence count."
+            />
+    <meta name="keywords" content="word, counter, document, literature, report,
+            assignment, writing, literature, text, grammar" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0,
+            user-scalable=true, minimum-scale=0.25, maximum-scale=2.0" />
+
+    <!-- title -->
+    <title>Word Counter</title>
+
+    <!-- third libraries -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <!-- project font... -->
+    <!-- font awesome... -->
+
+    <!-- first party resources -->
+</head>
+<body>
+    
+</body>
+</html>
