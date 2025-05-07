1ec4a12a518d8dedfa2c79e5ae267f47d71dccb3
Lyndon Panton <LyndonPanton@gmail.com>
2025-05-07 12:12:32 +0100

    Word count is displayed

--- web/index.php
+++ web/index.php
@@ -26,7 +26,6 @@
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Afacad+Flux:wght@100..1000&family=DM+Sans:ital,opsz,wght@0,9..40,100..1000;1,9..40,100..1000&family=Jost:ital,wght@0,100..900;1,100..900&family=Rethink+Sans:ital,wght@0,400..800;1,400..800&display=swap" rel="stylesheet">
-
     <!-- font awesome... -->
 
     <!-- first party resources -->
@@ -43,7 +42,7 @@
         <section id="counter">
             <ul>
                 <li>Word count: <span id="word-count">0</span></li>
-                <li>Character count: <span id="character-count">0</span></li>
+                <li>Letter count: <span id="letter-count">0</span></li>
                 <!-- <li>Space count: </li> -->
                 <!-- list common words, maybe skip words less than 4 characters
                         long -->
--- web/js/script.js
+++ web/js/script.js
@@ -1,10 +1,41 @@
 document.addEventListener("DOMContentLoaded", function (e) {
     console.log("DOM content has been loaded");
 
+
     // Add a 1 second delay between calculating the new count(s)
     // Delete the current timeout when the input area is updated
     let wordInput = document.getElementById("word-input");
+    wordInput.addEventListener("keypress", calculateStatistics);
+
+    let wordCount = document.getElementById("word-count");
+    let letterCount = document.getElementById("letter-count");
 
     // Use web storage to retain user input on load (save after word count is
     // calculated)
+    calculateStatistics(wordInput.value);
+
+    function calculateStatistics() {
+        // console.log(wordInput.value);
+        let words = wordInput.value.replace(/\s\s+/g, " ").split(" ");
+        console.log(words);
+
+        calculateWordCount(words);
+    }
+
+    function calculateLetterCount(e) {
+
+    }
+
+    function calculateSpaceCount(e) {
+
+    }
+
+    function calculateWordCount(words) {
+        wordCount.textContent = words.length;
+        console.log(words.length);
+    }
+
+    function saveStatistics(e) {
+
+    }
 });
